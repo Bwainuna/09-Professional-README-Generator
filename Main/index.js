807959ae6e306e319ff58ff7129025fb38f23570
Bwainuna <blakebah2@gmail.com>
2023-10-21 17:11:12 -0600

    added the questions and functions into the index.js, readme can now be generated

--- Main/index.js
+++ Main/index.js
@@ -1,13 +1,83 @@
-// TODO: Include packages needed for this application
+// Here are all the required files
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('../generateMarkdown');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// My prompts and questions for the User Input to be plugged into the README
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'Enter your project title:',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Enter a project description:',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Provide installation instructions:',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Explain how to use your project:',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Choose a license for your project:',
+    choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'None'],
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'Explain how others can contribute to your project:',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Provide test instructions:',
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'Enter your GitHub username:',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter your email address:',
+  },
+];
 
-// TODO: Create a function to initialize app
-function init() {}
+// Write to file function that generates the readme based on the user input data.
 
-// Function call to initialize app
-init();
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log('README.md has been successfully generated!');
+    }
+  });
+}
+
+// function that plugs the prompts into the generateMarkdown's format.
+
+function init() {
+  inquirer
+    .prompt(questions)
+    .then((answers) => {
+      const readmeContent = generateMarkdown(answers);
+      writeToFile('README.md', readmeContent);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+init(); // Start the application
